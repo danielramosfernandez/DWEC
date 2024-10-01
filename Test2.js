502f9fbf1eeb2197d5683b953c509a0effd820b2
Daniel Ramos <danidrf06@gmail.com>
2024-10-01 09:25:19 +0200

    Cuarto commit

--- Test2.js
+++ Test2.js
@@ -2,11 +2,16 @@ function testUnidad2(){
     var cadena= "abc\n";
     var cadena2= String("abc\n");
     var cadena3= new String("abc\n");
+    //console.log(typeof "abc");
     document.write(typeof cadena); 
-    document.write("<br/>")
+    document.write("<br/>");
     document.write(typeof cadena2); 
-
+    document.write("<br/>");
     document.write(typeof cadena3); 
-
+    document.write("<br/>");
     document.write(typeof cadena3.valueOf());
+    document.write("<br/>"); 
+    let longitud =10; 
+    longitud="10"; 
+    document.write(typeof longitud);
 }
