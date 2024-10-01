542c7eb6137facb39c1ea49dcabf1553ef83175f
Daniel Ramos <danidrf06@gmail.com>
2024-10-01 11:11:00 +0200

    Undecimo commit

---
+++ Ejercicios1.js
@@ -0,0 +1,10 @@
+function dosUno(){
+document.write("Hola mundo"); 
+} 
+function dosTres(){ 
+let num1=10; 
+let num2=5; 
+let divi=0; 
+divi=num1/num2; 
+document.write("El resultado es ", divi);
+}
