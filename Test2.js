c61f0a3f2288e033f342aecabda234f6d0ef6213
Daniel Ramos <danidrf06@gmail.com>
2024-10-04 11:07:34 +0200

    WOMBAT

--- Test2.js
+++ Test2.js
@@ -123,4 +123,28 @@ function testUnidad2_1(){
         alert(nombre+ ' está aprobado con un ' + nota);
     }
 }
+function testTest(){ 
+    let valor; 
+    do{ 
+        valor= prompt('Ingrese un valor entre 0 y 999: ', ''); 
+        valor=parseInt(valor); 
+        document.write('El valor ' + valor + 'tiene'); 
+        if (valor<0 || valor >999){ 
+            document.write("El número no está en rango ")
+        }
+        if (valor<10)
+            document.write('Tiene 1 dígito'); 
+        else{ 
+            if(valor<100)
+                document.write('Tiene 2 dígitos'); 
+            else if (valor<999) 
+                document.write('Tiene 3 digitos');
+            else
+            document.write('El número no esta en el rango elegido');
+        } 
+        document.write('<br>  />'); 
+        
+    }
+    while(valor>0 && valor<=999);
+}
 
