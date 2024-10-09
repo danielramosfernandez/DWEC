330a39c4d81807beb671042af7869699b341ee47
Daniel Ramos <danidrf06@gmail.com>
2024-10-09 10:23:06 +0200

    Miercoles

--- Practica1/Ejercicios1.js
+++ Practica1/Ejercicios1.js
@@ -1,20 +1,3 @@
 function dosUno(){
 alert("Hola mundo");
 }
-
-
-
-
-function dosDiez(){ 
-    let num = parseInt(prompt("Introduce un número para iniciar la cuenta atrás"));
-    if (isNaN(num)||num<=0){ 
-        alert("Ingrese un número mayor a 0.");
-    }else{ 
-        while (num>=0){ 
-            document.write("Número actual ",num)
-            num--; 
-        }
-        alert("Cuenta atras ")
-    }
-
-}
